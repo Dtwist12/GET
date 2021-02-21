774282b7c11d9c0c85e36813ce10554bb9645c4d
Joy Gogna <dasani407@gmail.com>
2021-02-21 10:12:09 -0500

    first commit

---
+++ super.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head> 
+<meta charset="UTF-8">
+
+    <title>Document</title>
+</head>
+<body>
+    <h1> $_GET demonstration </h1>
+<h2> Movie Title: <?php echo htmlspecialchars($_GET['title']); ?></h2>
+<h3> year released: <?php echo htmlspecialchars($_GET['year']);?> </h3>
+<h4> revenue: <?php echo htmlspecialchars($_GET['Revenue']);?></h4>
+<?php echo '<img src=  htmlspecialchars($_GET["https://target.scene7.com/is/image/Target/GUEST_5fb95e9e-f3b4-4ed6-a3fa-9fea45d23681?wid=488&hei=488&fmt=pjpeg"])' ?>
+
+</body>
+</html>
